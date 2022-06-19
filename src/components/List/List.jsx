5931bceda1d53e0f76a26fb909af8623c02944a5
Bathsheba Omidiora <shebadiora@gmail.com>
2022-06-19 18:56:28 -0500

    deployed

---
+++ src/components/List/List.jsx
@@ -0,0 +1,71 @@
+import React, { useState, useEffect, createRef } from "react";
+import {
+  CircularProgress,
+  Grid,
+  Typography,
+  InputLabel,
+  MenuItem,
+  FormControl,
+  Select,
+} from "@material-ui/core";
+import useStyles from "./styles";
+import PlaceDetails from "../PlaceDetails/PlaceDetails";
+
+const List = ({ places, childClicked, isLoading, rating, setRating, type, setType }) => {
+  const classes = useStyles();
+  const [eleRefs, setEleRefs] = useState([]);
+
+  useEffect(() => {
+    const refs = Array(places?.length)
+      .fill()
+      .map((_, idx) => eleRefs[idx] || createRef());
+
+    setEleRefs(refs);
+  }, [places]);
+
+  return (
+    <div className={classes.container}>
+      <Typography variant="h4">
+        Restaurants, Hotels & Attractions near you
+      </Typography>
+      {isLoading ? (
+        <div className={classes.loading}>
+          <CircularProgress size="5rem" />
+        </div>
+      ) : (
+        <>
+          <FormControl className={classes.formControl}>
+            <InputLabel>Type</InputLabel>
+            <Select value={type} onChange={(e) => setType(e.target.value)}>
+              <MenuItem value="restaurants">Restaurants</MenuItem>
+              <MenuItem value="hotels">Hotels</MenuItem>
+              <MenuItem value="attractions">Attractions</MenuItem>
+            </Select>
+          </FormControl>
+          <FormControl className={classes.formControl}>
+            <InputLabel>Rating</InputLabel>
+            <Select value={rating} onChange={(e) => setRating(e.target.value)}>
+              <MenuItem value={0}>All</MenuItem>
+              <MenuItem value={3}>Above 3.0</MenuItem>
+              <MenuItem value={4}>Above 4.0</MenuItem>
+              <MenuItem value={4.5}>Above 4.5</MenuItem>
+            </Select>
+          </FormControl>
+          <Grid container spacing={3} className={classes.list}>
+            {places?.map((place, idx) => (
+              <Grid ref={eleRefs[idx]} item key={idx} xs={12}>
+                <PlaceDetails
+                  place={place}
+                  selected={Number(childClicked) === idx}
+                  refProp={eleRefs[idx]}
+                />
+              </Grid>
+            ))}
+          </Grid>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default List;
